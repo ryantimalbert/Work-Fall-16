5cf33d64c727d6f88e03d3848b70a61717e7b695
Ryan Albert <rtalbert@genepool1.nersc.gov>
2016-09-20 14:39:15 -0700

    here

---
+++ GC_Content.py
@@ -0,0 +1,59 @@
+import MySQLdb as SQL
+import time
+import sys
+def GC_content(transcription):
+	n_count = 0
+	GC_count = 0
+	for nucleotide in transcription:
+		if nucleotide.lower() == "g" or nucleotide.lower() == "c":
+			GC_count += 1
+		elif nucleotide.lower() == "n":
+			n_count += 1
+		else:
+			pass
+	result = float(GC_count) / (len(transcription) - n_count)
+	return round(result * 100, 3)
+def Compile_Transcript(connection):
+	cursor = connection.cursor()
+	cursor.execute("Select seqTranscript from transcript;")
+	total_transcript = ""
+	for transcript in cursor.fetchall():
+		total_transcript += transcript[0]
+	cursor.close()
+	connection.close()
+	return total_transcript
+def total_breakdown(transcript):
+	A_count = 0
+	C_count = 0
+	G_count = 0
+	T_count =  0
+	for nucleo in transcript:
+		if nucleo.lower() == "a":
+			A_count += 1
+		elif nucleo.lower() == "g":
+			G_count += 1
+		elif nucleo.lower() == "t":
+			T_count  += 1
+		elif nucleo.lower() == "c":
+			C_count += 1
+		else:
+			pass
+	file = open('Total_Breakdown/{database}.txt'.format(database = name), 'w+')
+	total = A_count + T_count + C_count + G_count
+	A_number = float(A_count)/ total
+	file.write('A {num}\n'.format(num = A_number))
+	G_number = float(G_count)/ total
+	file.write('G {num}\n'.format(num = G_number))
+	T_number = float(T_count)/ total
+	file.write('T {num}\n'.format(num = T_number))
+	C_number = float(C_count)/ total
+	file.write('C {num}\n'.format(num = C_number))
+	file.close()
+
+name = sys.argv[1]
+connect= SQL.connect("gpdb02", "rtalbert", "Spike123@@", name)
+transcript_con = Compile_Transcript(connect)
+GC = GC_content(transcript_con)
+total_breakdown(transcript_con) ## Used for conservation around ATG
+file = open('GC_Content_File.txt', 'a')
+file.write("{database} GC Content: {value}%\n".format(database = name, value = str(GC)))
