87c337f6fba2ebf82273f06a56e644f57e138c9f
Ryan Albert <rtalbert@153.44.3.128.in-addr.dhcp.lbl.gov>
2016-09-29 15:33:57 -0700

    data

--- Solo_CP_Prediction_Test.py
+++ Solo_CP_Prediction_Test.py
@@ -4,6 +4,7 @@ import sys
 import time
 from sklearn.ensemble import RandomForestClassifier
 import csv
+import numpy
 # database_file = open('TestingDBs.txt', 'r')
 # genomes = database_file.readlines()
 # function_results = {}
@@ -42,16 +43,26 @@ import csv
 file = open('Compare_CP', 'r')
 function_results = pickle.load(file)
 file.close()
-file = open('Scatter_CP_VS_Function.csv', 'wb')
-writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-for name in function_results:
-	internal_array = []
-	data_array = function_results[name]
-	print(name)
-	print(data_array[0])
-	print(data_array[1])
-	internal_array.append(name)
-	internal_array.append(data_array[0] * 100)
-	internal_array.append(data_array[1] * 100)
-	writer.writerow(internal_array)
-file.close()
+Composite_data = []
+CP_data = []
+for genome in function_results:
+	data = function_results[genome]
+	Composite_data.append(data[0])
+	CP_data.append(data[1])
+print(numpy.mean(Composite_data))
+print(numpy.std(Composite_data))
+print(numpy.mean(CP_data))
+print(numpy,std(CP_data))
+# file = open('Scatter_CP_VS_Function.csv', 'wb')
+# writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+# for name in function_results:
+# 	internal_array = []
+# 	data_array = function_results[name]
+# 	print(name)
+# 	print(data_array[0])
+# 	print(data_array[1])
+# 	internal_array.append(name)
+# 	internal_array.append(data_array[0] * 100)
+# 	internal_array.append(data_array[1] * 100)
+# 	writer.writerow(internal_array)
+# file.close()
