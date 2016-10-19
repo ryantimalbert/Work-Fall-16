378aaf575cb023151fd39aaaa683a6bc32f7fea1
Ryan Albert <ryantimalbert@berkeley.edu>
2016-10-19 12:50:32 -0700

    feature selection

---
+++ Feature_Selection.py
@@ -0,0 +1,45 @@
+import sys
+from sklearn.ensemble import ExtraTreesClassifier
+p_fam_table = sys.argv[1]
+table = open(p_fam_table, 'r')
+lines = table.readlines()
+table.close()
+PFAM_Parse = []
+for line in lines:
+	line = line.split()
+	PFAM_Parse.append([line[0], line[2 ::]])
+### 98 different features for selection
+index = PFAM_Parse[0][1]
+features = PFAM_Parse[1 ::]
+Genomes = {}
+for count in range(len(index)):
+	genome = index[count]
+	Genomes[genome] = [[]]
+	for line in features:
+		Genomes[genome][0].append(line[1][count])
+life_style = sys.argv[2]
+style = open(life_style, 'r')
+lines = style.readlines()
+count = 0
+for line in lines[1::]:
+	line = line.split()
+	genome = line[0]
+	if genome == '##OUTGROUP':
+		continue;
+	if 'saprophyte' in line or 'saprophytic' in line:
+		Genomes[genome].append(1)
+	else:
+		Genomes[genome].append(0)
+target = []
+features = []
+for genome in Genomes:
+	target.append(Genomes[genome][1])
+	features.append(Genomes[genome][0])
+clf = ExtraTreesClassifier()
+clf = clf.fit(features, target)
+weight_array = clf.feature_importances_
+print(weight_array)
+
+
+
+
