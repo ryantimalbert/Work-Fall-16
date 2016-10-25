06124438182584494220fd99e92531444a89a2a9
Ryan Albert <ryantimalbert@berkeley.edu>
2016-10-25 01:42:11 -0700

    feature selection

--- Feature_Selection.py
+++ Feature_Selection.py
@@ -110,7 +110,7 @@ std1 = numpy.std(new_weights)
 for count in range(len(new_weights)):
 	if new_weights[count] >= mean1 + std1:
 		print(valued_p_fam[count]
-mean1 = numpy.mean(new_weights2)
+
 std1 = numpy.std(new_weights2)
 for count in range(len(new_weights2)):
 	if new_weights2[count] >= mean1 + std1:
