8402fef0b238c0e599cf8b2d669c89eac327bd8f
Ryan Albert <ryantimalbert@berkeley.edu>
2016-11-03 14:05:10 -0700

    test

--- Feature_Selection1.py
+++ Feature_Selection1.py
@@ -91,6 +91,8 @@ std1 = numpy.std(new_weights)
 for count in range(len(new_weights)):
 	if new_weights[count] >= mean1 + (2 * std1):
 		print(valued_p_fam[count])
+for val in valued_p_fam:
+	print(val)
 # for num in weight_array:
 # 	if num != 0:
 # 		count2 = 0
