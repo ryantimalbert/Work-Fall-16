0c5a540940ea4886123ae8e8f60bdea9c39188e1
Ryan Albert <rtalbert@145.44.3.128.in-addr.dhcp.lbl.gov>
2016-11-03 16:48:11 -0700

    data

--- Feature_Selection1.py
+++ Feature_Selection1.py
@@ -74,7 +74,6 @@ for genome in Genomes:
 		count1 += 1
 print(count1)
 print(len(features))
-PFAM = PFAM_Parse[1 ::]
 X_new = SelectKBest(k=100)
 X_new = X_new.fit(features, target)
 correct = X_new.get_support()
