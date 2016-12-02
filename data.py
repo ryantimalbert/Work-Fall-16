19d60e8aee48ad2ad28c34fc5d0e0dad95ae97fd
Ryan Albert <rtalbert@170.44.3.128.in-addr.dhcp.lbl.gov>
2016-12-01 16:49:54 -0800

    data

--- data.py
+++ data.py
@@ -2,9 +2,17 @@ import cPickle as pickle
 import os
 current_data = os.listdir('data')
 current_data = [data[0 : -4] for data in current_data]
+total = []
+families = []
 for family in current_data:
 	file = open('data/{fam}.txt'.format(fam = family), 'rb')
 	data_array = pickle.load(file)
 	file.close()
-	print(data_array)
-	print(family)
+	if 'eae' in family:
+		families.append(data_array[0])
+	total.append(data_array[0])
+import numpy
+print(total.mean())
+print(total.std())
+print(families.mean())
+print(families.std())
