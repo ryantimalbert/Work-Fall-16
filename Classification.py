1071ce46a147ab51082a8ee3364d3a34a01fba9d
Ryan Albert <ryantimalbert@berkeley.edu>
2016-12-11 17:40:11 -0800

    final project

--- Classification.py
+++ Classification.py
@@ -52,16 +52,34 @@ print(score.std())
 X_new = SelectKBest(k=9)
 X_new = X_new.fit(new_features, target)
 correct = X_new.get_support()
+scores = X_new.scores_
 count = 0
 best_features = []
+feature_index = []
+best_scores = []
 for i in features:
 	best_features.append([])
 for bol in correct:
 	if bol:
-		print(count)
+		feature_index.append(count)
+		best_scores.append(scores[count])
 		for count2 in range(len(new_features)):
 			best_features[count2].append(new_features[count2][count])
 	count += 1
+top_9 = []
+for count in range(len(best_scores)):
+	top_9.append((feature_index[count], best_scores[count]))
+top_9 = sorted(top_9, key = lambda k : k[1])
+top_9.reverse()
+for i in top_9:
+	print(i)
+clf = svm.SVC()
+score = cross_validation.cross_val_score(clf, best_features, target, cv = 5)
+print(score.mean())
+print(score.std())
+
+for b in best_features:
+	b[22] = b[22] * 3
 clf = svm.SVC()
 score = cross_validation.cross_val_score(clf, best_features, target, cv = 5)
 print(score.mean())
